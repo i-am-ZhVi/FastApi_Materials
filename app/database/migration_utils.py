ce5220a1ca5467e2575906302892553ec8c4f0ca
i_am_ZhVi <unius6ls66@gmail.com>
2025-10-25 18:03:07 +0300

    writing comments for database

--- app/database/migration_utils.py
+++ app/database/migration_utils.py
@@ -8,21 +8,39 @@ from sqlalchemy.ext.asyncio import create_async_engine
 __config_path__ = "alembic.ini"
 __migration_path__ = "migrations"
 
-cfg = Config(__config_path__)
-cfg.set_main_option("script_location", __migration_path__)
 
+cfg = Config(__config_path__) # обьект конфигурации алембика для работы с миграциями
+cfg.set_main_option("script_location", __migration_path__) # установка пути до директории алембика
+
+
+# применение миграций
+async def run_migrations(conn_url: str):
+    async_engine = create_async_engine(conn_url, echo=True) # генерация двигателя для взаимодействия с базой данных
+    async with async_engine.begin() as conn: # подключение к базе данных
+        await conn.run_sync(__execute_upgrade) # синхронный запуск мигрирования
 
 def __execute_upgrade(connection):
-    cfg.attributes["connection"] = connection
-    command.upgrade(cfg, "head")
+    cfg.attributes["connection"] = connection # вставляем подключение к базе данных в атрибуты
+    command.upgrade(cfg, "head") # мигрирование до последней версии
 
-async def run_migrations(conn_url: str):
-    async_engine = create_async_engine(conn_url, echo=True)
-    async with async_engine.begin() as conn:
-        await conn.run_sync(__execute_upgrade)
 
-async def create_migration(message: str):
-    command.revision(cfg, message=message, autogenerate=True)
+# создание новой миграции
+async def create_migration(conn_url, message: str):
+    async_engine = create_async_engine(conn_url, echo=True) # генерация двигателя для взаимодействия с базой данных
+    async with async_engine.begin() as conn: # подключение к базе данных
+        await conn.run_sync(__execute_create, message) # синхронный запуск создания миграции
 
-async def check_migration_status():
-    command.current(cfg)
+def __execute_create(connection, message):
+    cfg.attributes["connection"] = connection # вставляем подключение к базе данных в атрибуты
+    command.revision(cfg, message=message, autogenerate=True) # создание новой миграции
+
+
+# проверка статуса
+async def check_migration_status(conn_url):
+    async_engine = create_async_engine(conn_url, echo=True) # генерация двигателя для взаимодействия с базой данных
+    async with async_engine.begin() as conn: # подключение к базе данных
+        await conn.run_sync(__execute_check) # синхронный запуск проверки
+
+def __execute_check(connection):
+    cfg.attributes["connection"] = connection # вставляем подключение к базе данных в атрибуты
+    command.current(cfg) # проверяем статус
--- app/database/session.py
+++ app/database/session.py
@@ -1,12 +1,13 @@
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
 from app.config import settings
 
-engine = create_async_engine(settings.DATABASE_URL, echo=True)
-AsyncSessionLocal = async_sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+engine = create_async_engine(settings.DATABASE_URL, echo=True) # генерация двигателя для взаимодействия с базой данных
+AsyncSessionLocal = async_sessionmaker(engine, class_=AsyncSession, expire_on_commit=False) # генерация фабрики для генерации сессий взаимодействия с базой данных
 
+# Получение сессии
 async def get_db() -> AsyncSession:
-    async with AsyncSessionLocal() as session:
+    async with AsyncSessionLocal() as session: # генерация сессии
         try:
-            yield session
+            yield session # возврацаем сессию
         finally:
-            await session.close()
+            await session.close() # закрываем сессию
